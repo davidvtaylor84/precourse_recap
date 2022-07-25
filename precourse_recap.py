a9a03d676b91113fa15d7996f944d4ba55bbf60c
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-07-25 16:44:06 +0100

    precourse recap using list of books

---
+++ precourse_recap.py
@@ -0,0 +1,5 @@
+def favBook (name, book, author):
+  print("My name is " + name + " and my favourite book is " + book + " by " + author)
+favBook("Jimmy", "On the Road", "Jack Kerouac")
+favBook("Sarah", "Piranesi", "Susanna Clarke")
+favBook("Steven", "Underworld", "Don Delillo")
